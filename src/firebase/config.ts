c97cdd981da7b5044f7d5f962a68858b0678fbe5
FRANCISCO SAN MARTIN <sanmartinfrancisco8@gmail.com>
2025-10-07 16:05:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3193836505-da060",
+  "appId": "1:179665586409:web:d0d3990e9e4e4f8cd40878",
+  "apiKey": "AIzaSyButI8QW6xrzvc9O4J_pPLs3azMmB-I0t0",
+  "authDomain": "studio-3193836505-da060.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "179665586409"
+};
